61eff08a51837890d6d13cb8e43438f6e4fc68ed
Saad-Bajwa <bajwa.saad1122@gmail.com>
2024-10-25 22:55:38 +0500

    That is the making structure of the pos by adding login,shared and core module to the code

---
+++ pos-Frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomButtonsComponent } from './component/custom-buttons/custom-buttons.component';
+
+
+
+@NgModule({
+  declarations: [
+    CustomButtonsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
